0928b6949e65b324c3e1ae140a4f6db208e48305
junjie <jun_jie1994@hotmail.com>
2020-11-02 10:22:39 +0800

    tinkergram

--- config/database.php
+++ config/database.php
@@ -42,8 +42,27 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
         'mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3308'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        /*'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => 'us-cdbr-east-02.cleardb.com',
@@ -61,7 +80,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ],
+        ],*/
 
         'pgsql' => [
             'driver' => 'pgsql',
---
+++ public/js/welcome.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".h-16")
+    .fadeIn({duration: 500})
+    .css("display","none")
+    .slideDown(500);
+});
